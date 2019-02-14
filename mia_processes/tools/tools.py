6ab3283bd5369d2c56ab173199bd5c0d29238e6c
Eric Condamine <37933899+servoz@users.noreply.github.com>
2019-02-14 13:31:05 +0100

    Update tools.py

--- mia_processes/tools/tools.py
+++ mia_processes/tools/tools.py
@@ -5,7 +5,6 @@ from nipype.interfaces.base import traits
 
 # MIA import
 from populse_mia.project.filter import Filter
-
 from mia_processes.process_mia import Process_Mia
 
                         ### Bricks/classes in this file: ###
