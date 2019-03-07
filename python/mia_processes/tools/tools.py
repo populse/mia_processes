6cfa231411bb2aa801ecea2371a1320b167ab10f
econdami <econdami@yahoo.fr>
2019-03-07 11:58:44 +0100

    Changes for the license

--- python/mia_processes/tools/tools.py
+++ python/mia_processes/tools/tools.py
@@ -1,3 +1,11 @@
+##########################################################################
+# mia_processes - Copyright (C) IRMaGe/CEA, 2018
+# Distributed under the terms of the CeCILL license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html
+# for details.
+##########################################################################
+
 import os
 
 # Trait import
