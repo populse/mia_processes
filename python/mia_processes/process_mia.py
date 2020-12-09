43dc081a06217d6e2001c3e78f95a867bec358bd
Denis Rivière <denis.riviere.gm@gmail.com>
2020-12-09 16:45:02 +0100

    move methods of ProcessMIA into Process_MIA
    we probably don't need to keep both classes. The remaining methods are oriented towards nipype interfaces management and output inheritance, which was mainly done in Process_MIA anyway.

--- python/mia_processes/process_mia.py
+++ python/mia_processes/process_mia.py
@@ -26,9 +26,6 @@ import traits.api as traits
 # Populse_MIA imports
 from populse_mia.user_interface.pipeline_manager.process_mia import ProcessMIA
 
-# Capsul imports
-from capsul.process.process import NipypeProcess
-
 
 class Process_Mia(ProcessMIA):
     """Class overriding the ProcessMIA class, in order to personalise 
@@ -68,6 +65,7 @@ class Process_Mia(ProcessMIA):
         """
         if self.change_dir:
             self.switch_to_scripts_dir()
+            self.manage_matlab_launch_parameters()
 
         self.run_process_mia()
 
@@ -106,9 +104,14 @@ class Process_Mia(ProcessMIA):
                 'inheritance_dict': self.inheritance_dict}
 
     def run_process_mia(self):
-        """Used for Matlab's config parameters."""
-        if self.change_dir:
-            self.manage_matlab_launch_parameters()
+        """
+        Implements specific runs for Process_Mia subclasses
+        """
+        pass
+
+    def list_outputs(self):
+        """Override the outputs of the process."""
+        self.relax_nipype_exists_constraints()
 
     def switch_to_scripts_dir(self):
         """Method that changes the current working directory to the scripts
@@ -148,61 +151,22 @@ class Process_Mia(ProcessMIA):
         except Exception as e:
             print('{0}: {1}'.format(e.__class__, e))
 
+    def relax_nipype_exists_constraints(self):
+        if hasattr(self, 'process') and hasattr(self.process, 'inputs'):
+            ni_inputs = self.process.inputs
+            for name, trait in ni_inputs.traits().items():
+                relax_exists_constraint(trait)
 
-class MiaNipypeProcess(Process_Mia, NipypeProcess):
-    '''
-    Process inheriting both Process_Mia and NipypeProcess. This class can be
-    subclassed for specialized nipype interfaces, and customized if needed.
-    The factory functions (capsul.process.nipype_process.nipype_factory) will
-    be called automatically and appropriately when new instances are built,
-    thanks to the overloaded __new__ operator.
+    def manage_matlab_launch_parameters(self):
+        """Set the Matlab's config parameters when a Nipype process is used.
 
-    Specialized subclasses should define:
-
-    * a "_nipype_class" class attribute containing the nipype interface
-    class to be used with this process class
-    * a  `__postinit__()` method which performs the desired customizations. It
-    is used more or less like the constructor, but is called after the
-    constructor and the `NipypeProcess` initialisation is done.
-    * They do not need to (maybe should not) define an `__init__` method.
-
-    See :class:`~mia_processes.preprocess.spm.spatial_preprocessing.Smooth` for
-    an example.
-    '''
-
-    def __new__(cls, *args, **kwargs):
-        '''
-        we need to refefine the __new__ method to use the nipype_factory()
-        function under the hood for new instances. This way new instances
-        specialization will be allowed in a __postinit__ method.
-        '''
-
-        from capsul.process.nipype_process import nipype_factory
-        if len(args) == 0:
-            interface = getattr(cls, '_nipype_class', None)
-            if interface is None:
-                print('Warning: process class', cls.__name__,
-                      'does not define a Nipype interface as it should in its '
-                      '"_nipype_class" class attribute. Using an arbitrary '
-                      'spm.Smooth interface')
-                from nipype.interfaces import spm
-                interface = spm.Smooth
-            instance = nipype_factory(interface(), base_class=cls)
-            # override direct nipype reference
-            instance.id = instance.__class__.__module__ + "." + instance.name
-            instance.__postinit__(*args, **kwargs)
-        else:
-            instance = super(MiaNipypeProcess, cls).__new__(cls, *args,
-                                                            **kwargs)
-            instance.__mianp_init_done__ = False
-
-        return instance
-
-    def __init__(self, *args, **kwargs):
-        if hasattr(self, '__mianp_init_done__') and self.__mianp_init_done__:
-            return
-
-        self.__mianp_init_done__ = True
-        super(MiaNipypeProcess, self).__init__(*args, **kwargs)
-
+        Called in bricks.
+        """
+        # Note: this is a non-general trick which should probably not be here.
+        if hasattr(self, "process") and hasattr(self.process, 'inputs') \
+                and hasattr(self, 'use_mcr'):
+            self.process.inputs.use_mcr = self.use_mcr
+            self.process.inputs.paths = self.paths
+            self.process.inputs.matlab_cmd = self.matlab_cmd
+            self.process.inputs.mfile = self.mfile
 
