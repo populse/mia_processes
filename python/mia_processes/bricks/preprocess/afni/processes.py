e7c74f1f3d9a4c6a0e669ec4b7fa669b2c26ece1
LStruber <lucas.struber@gmail.com>
2022-04-28 17:07:18 +0200

    'AFNI QC bricks'

--- python/mia_processes/bricks/preprocess/afni/processes.py
+++ python/mia_processes/bricks/preprocess/afni/processes.py
@@ -29,7 +29,7 @@ from populse_mia.user_interface.pipeline_manager.process_mia import ProcessMIA
 
 # Other import
 import os
-from traits.api import Bool, Enum, Float, Int, List, String, Undefined
+from traits.api import Bool, Either, Enum, Float, Int, List, String, Undefined
 import nibabel as nib
 
 
@@ -733,7 +733,7 @@ class DropTRs(ProcessMIA):
                 if self.out_prefix == Undefined:
                     self.out_prefix = 'cropped_'
                     print('The out_prefix parameter is undefined. Automatically '
-                          'set to "c" ...')
+                          'set to "cropped" ...')
 
                 if self.output_directory:
                     ifile = os.path.split(self.in_file)[-1]
@@ -799,6 +799,261 @@ class DropTRs(ProcessMIA):
 
         if self.out_prefix:
             self.process.out_prefix = self.out_prefix
+
+        return self.process.run(configuration_dict={})
+
+
+class FWHMx(ProcessMIA):
+    """
+    * Computes FWHMs for all sub-bricks in the input dataset, each one separately *
+
+    Please, see the complete documentation for the `FWHMx' brick in the populse.mia_processes website
+    <https://populse.github.io/mia_processes/documentation/bricks/preprocess/afni/FWHMx.html>`_
+
+    """
+
+    def __init__(self):
+        """Dedicated to the attributes initialisation/instanciation.
+
+        The input and output plugs are defined here. The special
+        'self.requirement' attribute (optional) is used to define the
+        third-party products necessary for the running of the brick.
+        """
+        # Initialisation of the objects needed for the launch of the brick
+        super(FWHMx, self).__init__()
+
+        # Third party softwares required for the execution of the brick
+        self.requirement = ['afni', 'nipype']
+
+        # Inputs description
+        in_file_desc = ('Input image (a pathlike object or string '
+                        'representing a file).')
+        mask_file_desc = ('Mask image (a pathlike object or string '
+                        'representing a file).')
+        combine_desc = 'Combine the final measurements along each axis (a bool).'
+        detrend_desc = 'detrend to the specified order (a bool or an int).'
+        out_prefix_desc = ('Specify the string to be prepended to the '
+                           'filenames of the output image file '
+                           '(a string).')
+
+        # Outputs description
+        out_file_desc = ('The output file (a pathlike object or a '
+                         'string representing a file).')
+
+        # Inputs traits
+        self.add_trait("in_file",
+                       File(output=False,
+                            optional=False,
+                            desc=in_file_desc))
+
+        self.add_trait("mask_file",
+                       File(Undefined,
+                            output=False,
+                            optional=True,
+                            desc=mask_file_desc))
+
+        self.add_trait("combine",
+                       Bool(True,
+                            optional=True,
+                            output=False,
+                            desc=combine_desc))
+
+        self.add_trait("detrend",
+                       Either(Bool,
+                              Int,
+                              default=True,
+                              output=False,
+                              desc=detrend_desc))
+
+        self.add_trait("out_prefix",
+                       String('fwhm_',
+                              output=False,
+                              optional=True,
+                              desc=out_prefix_desc))
+
+        # Outputs traits
+        self.add_trait("out_file",
+                       File(output=True,
+                            desc=out_file_desc))
+
+        self.init_default_traits()
+
+        self.init_process('nipype.interfaces.afni.FWHMx')
+
+    def list_outputs(self, is_plugged=None):
+        """Dedicated to the initialisation step of the brick.
+
+        The main objective of this method is to produce the outputs of the
+        bricks (self.outputs) and the associated tags (self.inheritance_dic),
+        if defined here. In order not to include an output in the database,
+        this output must be a value of the optional key 'notInDb' of the
+        self.outputs dictionary. To work properly this method must return
+        self.make_initResult() object.
+
+        :param is_plugged: the state, linked or not, of the plugs.
+        :returns: a dictionary with requirement, outputs and inheritance_dict.
+        """
+        # Using the inheritance to ProcessMIA class, list_outputs method
+        super(FWHMx, self).list_outputs()
+
+        # Outputs definition and tags inheritance (optional)
+        if self.in_file:
+
+            if self.out_prefix == Undefined:
+                self.out_prefix = 'fwhm_'
+                print('The out_prefix parameter is undefined. Automatically '
+                      'set to "fwhm" ...')
+
+            if self.output_directory:
+                ifile = os.path.split(self.in_file)[-1]
+
+                try:
+                    fileName, trail = ifile.rsplit('.', 1)
+                    if trail == 'gz':
+                        (fileName_2,
+                         trail_2) = os.path.splitext(fileName)
+                        if trail_2 == 'nii':
+                            trail = 'nii.gz'
+
+                except ValueError:
+                    print('\nThe input image format is not recognized ...!')
+                    return
+
+                else:
+                    self.outputs['out_file'] = os.path.join(
+                        self.output_directory,
+                        self.out_prefix + fileName + '.out')
+
+            else:
+                print('No output_directory was found...!\n')
+                return
+
+            self.inheritance_dict[self.outputs[
+                'out_file']] = self.in_file
+
+        # Return the requirement, outputs and inheritance_dict
+        return self.make_initResult()
+
+    def run_process_mia(self):
+        """Dedicated to the process launch step of the brick."""
+        super(FWHMx, self).run_process_mia()
+
+        self.process.in_file = self.in_file
+        self.process.mask = self.mask_file
+        self.process.combine = self.combine
+        self.process.detrend = self.detrend
+        self.process.out_file = self.out_file
+
+        if self.out_prefix:
+            self.process.out_prefix = self.out_prefix
+
+        return self.process.run(configuration_dict={})
+
+
+class GCOR(ProcessMIA):
+    """
+    * Computes the average correlation between every voxel and ever other voxel, over any give mask *
+
+    Please, see the complete documentation for the `GCOR' brick in the populse.mia_processes website
+    <https://populse.github.io/mia_processes/documentation/bricks/preprocess/afni/GCOR.html>`_
+
+    """
+
+    def __init__(self):
+        """Dedicated to the attributes initialisation/instanciation.
+
+        The input and output plugs are defined here. The special
+        'self.requirement' attribute (optional) is used to define the
+        third-party products necessary for the running of the brick.
+        """
+        # Initialisation of the objects needed for the launch of the brick
+        super(GCOR, self).__init__()
+
+        # Third party softwares required for the execution of the brick
+        self.requirement = ['afni', 'nipype']
+
+        # Inputs description
+        in_file_desc = ('Input image (a pathlike object or string '
+                        'representing a file).')
+        mask_file_desc = ('Mask image (a pathlike object or string '
+                        'representing a file).')
+        out_prefix_desc = ('Specify the string to be prepended to the '
+                           'filenames of the output image file '
+                           '(a string).')
+
+        # Outputs description
+        out_desc = 'Global correlation value (a float).'
+
+        # Inputs traits
+        self.add_trait("in_file",
+                       File(output=False,
+                            optional=False,
+                            desc=in_file_desc))
+
+        self.add_trait("mask_file",
+                       File(Undefined,
+                            output=False,
+                            optional=True,
+                            desc=mask_file_desc))
+
+        # Outputs traits
+        self.add_trait("out",
+                       Float(output=True,
+                             desc=out_desc))
+
+        self.init_default_traits()
+
+        self.init_process('nipype.interfaces.afni.GCOR')
+
+    def list_outputs(self, is_plugged=None):
+        """Dedicated to the initialisation step of the brick.
+
+        The main objective of this method is to produce the outputs of the
+        bricks (self.outputs) and the associated tags (self.inheritance_dic),
+        if defined here. In order not to include an output in the database,
+        this output must be a value of the optional key 'notInDb' of the
+        self.outputs dictionary. To work properly this method must return
+        self.make_initResult() object.
+
+        :param is_plugged: the state, linked or not, of the plugs.
+        :returns: a dictionary with requirement, outputs and inheritance_dict.
+        """
+        # Using the inheritance to ProcessMIA class, list_outputs method
+        super(GCOR, self).list_outputs()
+
+        # Outputs definition and tags inheritance (optional)
+        if self.in_file:
+
+            if self.output_directory:
+                ifile = os.path.split(self.in_file)[-1]
+
+                try:
+                    fileName, trail = ifile.rsplit('.', 1)
+                    if trail == 'gz':
+                        (fileName_2,
+                         trail_2) = os.path.splitext(fileName)
+                        if trail_2 == 'nii':
+                            trail = 'nii.gz'
+
+                except ValueError:
+                    print('\nThe input image format is not recognized ...!')
+                    return
+
+                else:
+                    self.outputs['out'] = 0
+            else:
+                print('No output_directory was found...!\n')
+                return
+
+        # Return the requirement, outputs and inheritance_dict
+        return self.make_initResult()
+
+    def run_process_mia(self):
+        """Dedicated to the process launch step of the brick."""
+        super(GCOR, self).run_process_mia()
+
+        self.process.in_file = self.in_file
+        self.process.mask = self.mask_file
 
         return self.process.run(configuration_dict={})
 
@@ -947,6 +1202,138 @@ class Mean(ProcessMIA):
         self.process.args = '-mean'
         self.process.outputtype = self.output_type
         self.process.out_file = self.out_file
+
+        if self.out_prefix:
+            self.process.out_prefix = self.out_prefix
+
+        return self.process.run(configuration_dict={})
+
+
+class QualityIndex(ProcessMIA):
+    """
+    * Computes a quality index for each sub-brick in a 3D+time dataset. The output is a 1D time
+    series with the index for each sub-brick *
+
+    Please, see the complete documentation for the `QualityIndex' brick in the populse.mia_processes website
+    <https://populse.github.io/mia_processes/documentation/bricks/preprocess/afni/QualityIndex.html>`_
+
+    """
+
+    def __init__(self):
+        """Dedicated to the attributes initialisation/instanciation.
+
+        The input and output plugs are defined here. The special
+        'self.requirement' attribute (optional) is used to define the
+        third-party products necessary for the running of the brick.
+        """
+        # Initialisation of the objects needed for the launch of the brick
+        super(QualityIndex, self).__init__()
+
+        # Third party softwares required for the execution of the brick
+        self.requirement = ['afni', 'nipype']
+
+        # Inputs description
+        in_file_desc = ('A bold file (a pathlike object or string '
+                        'representing a file).')
+        automask_desc = 'Clip off small voxels (a boolean)'
+        out_prefix_desc = ('Specify the string to be prepended to the '
+                           'filenames of the output image file '
+                           '(a string).')
+
+        # Outputs description
+        out_file_desc = ('The output file (a pathlike object or a '
+                         'string representing a file).')
+
+        # Inputs traits
+        self.add_trait("in_file",
+                       File(output=False,
+                            optional=False,
+                            desc=in_file_desc))
+
+        self.add_trait("automask",
+                       Bool(True,
+                            output=False,
+                            optional=False,
+                            desc=automask_desc))
+
+        self.add_trait("out_prefix",
+                       String('QI_',
+                              output=False,
+                              optional=True,
+                              desc=out_prefix_desc))
+
+        # Outputs traits
+        self.add_trait("out_file",
+                       File(output=True,
+                            desc=out_file_desc))
+
+        self.init_default_traits()
+
+        self.init_process('nipype.interfaces.afni.QualityIndex')
+
+    def list_outputs(self, is_plugged=None):
+        """Dedicated to the initialisation step of the brick.
+
+        The main objective of this method is to produce the outputs of the
+        bricks (self.outputs) and the associated tags (self.inheritance_dic),
+        if defined here. In order not to include an output in the database,
+        this output must be a value of the optional key 'notInDb' of the
+        self.outputs dictionary. To work properly this method must return
+        self.make_initResult() object.
+
+        :param is_plugged: the state, linked or not, of the plugs.
+        :returns: a dictionary with requirement, outputs and inheritance_dict.
+        """
+        # Using the inheritance to ProcessMIA class, list_outputs method
+        super(QualityIndex, self).list_outputs()
+
+        # Outputs definition and tags inheritance (optional)
+        if self.in_file:
+
+            if self.out_prefix == Undefined:
+                self.out_prefix = 'QI_'
+                print('The out_prefix parameter is undefined. Automatically '
+                      'set to "QI" ...')
+
+            if self.output_directory:
+                ifile = os.path.split(self.in_file)[-1]
+
+                try:
+                    fileName, trail = ifile.rsplit('.', 1)
+                    if trail == 'gz':
+                        (fileName_2,
+                         trail_2) = os.path.splitext(fileName)
+                        if trail_2 == 'nii':
+                            trail = 'nii.gz'
+
+                except ValueError:
+                    print('\nThe input image format is not recognized ...!')
+                    return
+
+                else:
+                    self.outputs['out_file'] = os.path.join(
+                        self.output_directory,
+                        self.out_prefix + fileName + '.txt')
+                    print('\nThe input image format does not seem to be '
+                          'nii or 3D. This can prevent the process '
+                          'launch ...!')
+
+                    self.inheritance_dict[self.outputs[
+                        'out_file']] = self.in_file
+
+            else:
+                print('No output_directory was found...!\n')
+                return
+
+        # Return the requirement, outputs and inheritance_dict
+        return self.make_initResult()
+
+    def run_process_mia(self):
+        """Dedicated to the process launch step of the brick."""
+        super(QualityIndex, self).run_process_mia()
+
+        self.process.in_file = self.in_file
+        self.process.automask = self.automask
 
         if self.out_prefix:
             self.process.out_prefix = self.out_prefix
