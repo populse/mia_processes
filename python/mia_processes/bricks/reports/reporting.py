198a25306d2ce6a91decc300e11132a06b7bf6cb
econdami <econdami@yahoo.fr>
2022-04-28 17:30:49 +0200

    work on the automatic report for MRIQC

--- python/mia_processes/bricks/reports/reporting.py
+++ python/mia_processes/bricks/reports/reporting.py
@@ -20,21 +20,34 @@ generate automatic report at the end of a pipeline calculation.
 ##########################################################################
 
 # nibabel import
-#import nibabel as nib
+import nibabel as nib
 
 # nipype import
-#from nipype.interfaces.base import (OutputMultiPath, InputMultiPath, File,
-#                                    traits, TraitListObject, Undefined,
-#                                    DictStrStr, Str)
+from nipype.interfaces.base import (OutputMultiPath, InputMultiPath, File,
+                                    traits, TraitListObject, Undefined,
+                                    DictStrStr, Str)
+from nipype.interfaces.spm.base import ImageFileSPM
 
 # populse_mia import
+from populse_mia import sources_images
+from populse_mia.data_manager.project import COLLECTION_CURRENT
 from populse_mia.user_interface.pipeline_manager.process_mia import ProcessMIA
 
+
+
 # soma-base imports
-#from soma.qt_gui.qt_backend.Qt import QMessageBox
+from soma.qt_gui.qt_backend.Qt import QMessageBox
+
 
 # Other import
-#import os
+import os
+from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER, TA_LEFT, TA_RIGHT
+from reportlab.lib.pagesizes import A4, landscape, portrait
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import mm
+from reportlab.lib import colors
+from reportlab.platypus import (SimpleDocTemplate, Paragraph, Spacer, Image,
+                                 Table, TableStyle, PageBreak)
 
 
 class MRIQC_report(ProcessMIA):
@@ -60,13 +73,32 @@ class MRIQC_report(ProcessMIA):
         self.requirement = []
 
         # Inputs description
+        anat_desc = ('An existing, uncompressed anatomic image file (valid '
+                       'extensions: .nii)')
 
         # Outputs description
+        report_desc = 'The generated report (pdf)'
 
         # Inputs traits
-
+        self.add_trait("anat",
+                       ImageFileSPM(copyfile=False,
+                                    output=False,
+                                    optional=False,
+                                    desc=anat_desc))
         # Outputs traits
+        self.add_trait("report",
+                       OutputMultiPath(File(),
+                                       output=True,
+                                       optional=True,
+                                       desc=report_desc))
 
+        self.add_trait("patient_name",
+                      traits.Either(Undefined,
+                                    Str(),
+                                    output=False,
+                                    optional=False,
+                                    userlevel=1))
+        self.patient_name = Undefined
         self.init_default_traits()
 
     def list_outputs(self, is_plugged=None):
@@ -83,9 +115,81 @@ class MRIQC_report(ProcessMIA):
         # Using the inheritance to ProcessMIA class, list_outputs method
         super(MRIQC_report, self).list_outputs()
 
+
+
+        if self.anat and self.anat not in ["<undefined>", traits.Undefined]:
+            self.outputs['report'] = 'report.pdf'
+
+
+
+        file_position = (self.anat.find(self.project.getName())
+                         + len(self.project.getName()) + 1)
+        database_filename = self.anat[file_position:]
+
+        if ('PatientName' in
+                self.project.session.get_fields_names(COLLECTION_CURRENT)):
+            self.patient_name = self.project.session.get_value(COLLECTION_CURRENT,
+                                                  database_filename,
+                                                  ('PatientName'))
+
         # Return the requirement, outputs and inheritance_dict
         return self.make_initResult()
 
     def run_process_mia(self):
         """Dedicated to the process launch step of the brick."""
         super(MRIQC_report, self).run_process_mia()
+
+
+        print('self.patient_name: ', self.patient_name)
+        header_image = os.path.join(sources_images.__path__[0],
+                                    'Logo_populse_mia_LR.jpeg')
+
+
+
+
+
+
+
+        title = '<font size=18> <b> %s report: </b> %s <br/> <br/></font>' % (patient_ref, aujourdhui.strftime('%Y.%m.%d'))     # Title:  patient_ref + report - running date.
+        #Canvas creation
+        rapport = []
+
+        # First page - cover
+
+        im_entete.hAlign = 'CENTER'
+        rapport.append(im_entete)
+
+        rapport.append(Spacer(0 * mm, 10 * mm))  # (width, height)
+
+        rapport.append(Paragraph(nomDuSuperLogiciel,
+                                 styles['Center']))
+
+        rapport.append(Spacer(0 * mm, 4 * mm))  # (width, height)  ## In Linux
+        # rapport.append(Spacer(0*mm, 4*mm)) # (width, height)  ## In Mac
+
+        line = ReportLine(250)
+        line.hAlign = 'CENTER'
+        rapport.append(line)
+
+        rapport.append(Spacer(0 * mm, 20 * mm))
+
+        rapport.append(Paragraph(titre,
+                                 styles['Center']))
+
+        rapport.append(Spacer(0 * mm, 10 * mm))
+
+        data.setStyle(TableStyle([
+            ('LINEABOVE', (0, 0), (-1, 0), 2, colors.black),
+            ('LINEBELOW', (0, -1), (-1, -1), 2, colors.black),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE')
+        ]))  # Carefull: Table use (col,raw) and 0-base for top left start as usual OR -1-base for lower right start.
+
+        data.hAlign = 'CENTER'
+        rapport.append(data)
+
+        rapport.append(Spacer(0 * mm, 10 * mm))
+
+        rapport.append(Paragraph(texteDisclaimer,
+                                 styles["Justify"]))
+
+        rapport.append(PageBreak())
