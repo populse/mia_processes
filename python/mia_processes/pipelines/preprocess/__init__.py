26ba60ac669a9874b31ff85f9024e762834dfdb4
LStruber <lucas.struber@gmail.com>
2022-05-02 13:30:42 +0200

    'Anat QC init fix'

--- python/mia_processes/pipelines/preprocess/__init__.py
+++ python/mia_processes/pipelines/preprocess/__init__.py
@@ -11,6 +11,7 @@
 from .spatial_preprocessing import Spatial_preprocessing
 from .anat_airmask_pipeline import Anat_airmask_pipeline
 from .anat_headmask_pipeline import Anat_headmask_pipeline
+from .anat_mni_tpms_pipeline import Anat_mni_tpms_pipeline
 from .anat_skullstrip_pipeline import Anat_skullstrip_pipeline
 from .anat_spatial_norm import Anat_spatial_norm
 from .bold_hmc_pipeline import Bold_hmc_pipeline
