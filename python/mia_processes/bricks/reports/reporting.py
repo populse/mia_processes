dddae8941cee2b764d64cda4880bd6c0c31079ed
econdami <econdami@yahoo.fr>
2022-05-04 19:09:24 +0200

    work on the automatic report for MRIQC

--- python/mia_processes/bricks/reports/reporting.py
+++ python/mia_processes/bricks/reports/reporting.py
@@ -55,6 +55,10 @@ from reportlab.platypus import (SimpleDocTemplate, Paragraph, Spacer, Image,
                                  Table, TableStyle, PageBreak)
 import tempfile
 import platform
+import nibabel as nib
+from nibabel.processing import resample_from_to
+import matplotlib.pyplot as plt
+import scipy.ndimage as ndi
 
 
 
@@ -129,12 +133,6 @@ class MRIQC_report(ProcessMIA):
         # Using the inheritance to ProcessMIA class, list_outputs method
         super(MRIQC_report, self).list_outputs()
 
-
-
-        if self.anat and self.anat not in ["<undefined>", traits.Undefined]:
-            self.outputs['report'] = os.path.join(self.output_directory, 'report.pdf')
-
-
         # As we do not have access to the database at the runtime (see #272),
         # we prepare here the data that the run_process_mia method will need
         # via dict4runtime dictionary
@@ -143,11 +141,21 @@ class MRIQC_report(ProcessMIA):
                          + len(self.project.getName()) + 1)
         database_filename = self.anat[file_position:]
 
+
         if ('PatientName' in
                 self.project.session.get_fields_names(COLLECTION_CURRENT)):
             self.dict4runtime['patient_name'] = self.project.session.get_value(COLLECTION_CURRENT,
                                                   database_filename,
                                                   ('PatientName'))
+
+        if self.dict4runtime['patient_name'] in ('', Undefined):
+            self.dict4runtime['patient_name'] = "Undefined_name_ref"
+
+        if self.anat and self.anat not in ["<undefined>", traits.Undefined]:
+            self.outputs['report'] = os.path.join(self.output_directory, self.dict4runtime['patient_name'] + '_mriqcReport_' + datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f')[:22] +'.pdf')
+
+
+
         # study_name
         if ('StudyName' in
                 self.project.session.get_fields_names(COLLECTION_CURRENT)):
@@ -206,11 +214,10 @@ class MRIQC_report(ProcessMIA):
         super(MRIQC_report, self).run_process_mia()
 
         header_image = os.path.join(sources_images.__path__[0],
-                                    'Logo_populse_mia_LR.jpeg')
-        header_image = Image(header_image, 175.0 * mm,
-                            30.0 * mm)  # 2508px × 430px
+                                    'Logo_populse_mia_HR.jpeg')
+        header_image = Image(header_image, 40.8 * mm, 30.0 * mm)  # 2505px x 1843px
 
-        header_title = '<font size=30><b>MRI &nbsp Q</b></font>  <font size=11>uality</font>   <font size=30><b>&nbsp C</b></font>   <font size=11>ontrol</font>'
+        header_title = '<font size=30><b>MRI&nbsp Q</b></font>  <font size=11>uality</font>   <font size=30><b>&nbsp C</b></font>   <font size=11>ontrol</font>'
 
         if 'site' in self.dict4runtime and self.dict4runtime['site'] not in ('', Undefined):
             site = self.dict4runtime['site']
@@ -248,14 +255,12 @@ class MRIQC_report(ProcessMIA):
         else:
             patient_age = "*Undefined patient age*"
 
-
-        run_date = datetime.now()
-
         with tempfile.NamedTemporaryFile() as temp_file:
-            temp_file.write(bytes("SITE:{}".format('site'), encoding='utf8'))
+            temp_file.write(bytes("SITE:{}".format(self.dict4runtime['site']), encoding='utf8'))
             temp_file.write(bytes("\nMRI SCANNER: {}".format(self.dict4runtime['mri_scanner']), encoding='utf8'))
             temp_file.write(bytes("\nSTUDY NAME: {}".format(study_name), encoding='utf8'))
             temp_file.write(bytes("\nEXAMINATION DATE: {}".format(acqu_date), encoding='utf8'))
+            temp_file.write(bytes("\nMRIQC CALCULATION DATE: {}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), encoding='utf8'))
             temp_file.write(bytes("\nPATIENT REFERENCE: {}".format(patient_ref), encoding='utf8'))
             temp_file.write(bytes("\nPATIENT SEX: {}".format(patient_sex), encoding='utf8'))
             temp_file.write(bytes("\nPATIENT AGE: {}".format(patient_age), encoding='utf8'))
@@ -280,8 +285,7 @@ class MRIQC_report(ProcessMIA):
 
             cover_data = recupCover(temp_file.name)
 
-        cover_data = Table(cover_data, [48*mm, 97*mm])  # colWidths, rowHeights
-
+        cover_data = Table(cover_data, [60*mm, 97*mm])  # colWidths, rowHeights
         output = self.report
 
         page = SimpleDocTemplate(output,
@@ -315,9 +319,7 @@ class MRIQC_report(ProcessMIA):
                                   bulletText=u'❍')
                                  )
 
-        title = ('<font size=18> <b> {0} report: </b> {1} '
-                 '<br/> <br/></font>').format(patient_ref,
-                                              run_date.strftime('%Y.%m.%d'))     # Title:  patient_ref + report - running date.
+        title = ('<font size=18> <b>Image-Quality Metrics summary report</b><br/> <br/></font>')
 
 
         textDisclaimer = ("<font size=7 ><i><b>DISCLAIMER</b><br/>Mia software,"
@@ -360,7 +362,7 @@ class MRIQC_report(ProcessMIA):
         report.append(Paragraph(header_title,
                                  styles['Center']))
 
-        report.append(Spacer(0 * mm, 4 * mm))  # (width, height)  ## In Linux
+        report.append(Spacer(0 * mm, 12 * mm))  # (width, height)  ## In Linux
         # report.append(Spacer(0*mm, 4*mm)) # (width, height)  ## In Mac
 
         line = ReportLine(250)
@@ -390,4 +392,48 @@ class MRIQC_report(ProcessMIA):
 
         report.append(PageBreak())
 
+        report.append(Paragraph("<font size = 18 > <b> MRI axial slice planes display <br/> </b> </font>",
+                                     styles['Center']))
+
+        report.append(Spacer(0*mm, 4*mm)) # (width, height) ## In Linux
+
+        line = ReportLine(150)
+        line.hAlign = 'CENTER'
+        report.append(line)
+
+        report.append(Spacer(0*mm, 25*mm)) # (width, height)
+
+        fig_rows = 5
+        fig_cols = 5
+        n_subplots = fig_rows * fig_cols
+        brain_img = nib.load(self.anat)
+        brain_voxels = brain_img.get_fdata()
+
+        # axial
+        n_slice = brain_voxels.shape[1]
+        step_size = n_slice // n_subplots
+        plot_range = n_subplots * step_size
+        start_stop = int((n_slice - plot_range) / 2)
+
+        tmpdir = tempfile.TemporaryDirectory()
+
+        fig, axs = plt.subplots(fig_rows, fig_cols, figsize=[10, 10])
+
+        for idx, img in enumerate(range(start_stop, plot_range, step_size)):
+            #axs.flat[idx].imshow(ndi.rotate(t1_voxels[:, img, :], 270),  cmap='gray')
+            axs.flat[idx].imshow(brain_voxels[:, img, :],  cmap='gray')
+            axs.flat[idx].axis('off')
+
+        plt.tight_layout()
+        #plt.show()
+        plt.savefig(os.path.join(tmpdir.name, 'slices.png'), bbox_inches='tight')
+
+        #slices_image = os.path.join(sources_images.__path__[0],
+        #                            'Logo_populse_mia_HR.jpeg')
+        slices_image = Image(os.path.join(tmpdir.name, 'slices.png'), 177.4 * mm, 222.0 * mm)  #791x990
+        slices_image.hAlign = 'CENTER'
+        report.append(slices_image)
+
+
         page.build(report, canvasmaker=PageNumCanvas)
+        tmpdir.cleanup()
