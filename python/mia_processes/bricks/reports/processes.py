c5166837468c1c87a7401881c9e14aa65a3a7d8d
manuegrx <47973414+manuegrx@users.noreply.github.com>
2023-03-01 14:30:33 +0100

    return -1 for fber indice when bg_mu really small

--- python/mia_processes/bricks/reports/processes.py
+++ python/mia_processes/bricks/reports/processes.py
@@ -3068,7 +3068,7 @@ def fber(img, headmask, rotmask=None):
         airmask[rotmask > 0] = 0
     bg_mu = np.median(np.abs(img[airmask == 1]) ** 2)
     if bg_mu < 1.0e-3:
-        return 0
+        return -1.0
     return float(fg_mu / bg_mu)
 
 
