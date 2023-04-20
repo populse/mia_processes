cb6f11ef99d87295e87e650e4b0ee3a14b3b8226
econdami <econdami@yahoo.fr>
2023-04-20 20:37:13 +0200

    work on documentation

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+8d51d6e',
+    VERSION: '2.2.1-dev+2d129ae',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
