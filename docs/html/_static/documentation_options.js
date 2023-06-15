c56e72fae362c8bf04da13e03ac64181580abb3c
manuegrx <47973414+manuegrx@users.noreply.github.com>
2023-06-15 15:24:52 +0200

    documentation work

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+ad90ff74',
+    VERSION: '2.2.1-dev+5241b0ab',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
