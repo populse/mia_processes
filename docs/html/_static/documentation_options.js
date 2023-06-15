5241b0ab6c2b40e53db8aec8ca8b75999b488a7d
manuegrx <47973414+manuegrx@users.noreply.github.com>
2023-06-15 15:17:30 +0200

    fix link to documentation

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+bb426005',
+    VERSION: '2.2.1-dev+ad90ff74',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
