c2abd03c10f0442fca2cd25f1ca0989ae76f0f83
manuegrx <47973414+manuegrx@users.noreply.github.com>
2023-10-11 16:33:02 +0200

    update dwi_tissue_boundaries pipeline

--- mia_processes/pipelines/preprocess/dwi_tissue_boundaries.py
+++ mia_processes/pipelines/preprocess/dwi_tissue_boundaries.py
@@ -16,10 +16,10 @@ from capsul.api import Pipeline
 class Dwi_tissue_boundaries(Pipeline):
 
     """
-    *DWI create tissue boundaries with a T1w*
+    *DWI create tissue boudadiries with a T1w*
 
     Please, see the complete documentation for the
-    `Dwi_tissue_boundaries pipeline in the mia_processes website
+    `Dwi_tissue_boundaries pipeline in the populse.mia_processes website
     <https://populse.github.io/mia_processes/html/documentation/pipelines/preprocess/Dwi_tissue_boundaries.html>`_
 
     """
@@ -42,6 +42,18 @@ class Dwi_tissue_boundaries(Pipeline):
         self.add_process(
             "Meanb0", "mia_processes.bricks.preprocess.mrtrix.processes.MRMath"
         )
+        self.add_process(
+            "Convert5ttNIfTI",
+            "mia_processes.bricks.preprocess.mrtrix.processes.MRConvert",
+        )
+        self.nodes["Convert5ttNIfTI"].process.out_file_format = "NIFTI"
+        self.add_process(
+            "ExtractROI",
+            "mia_processes.bricks.preprocess.fsl.processes.ExtractROI",
+        )
+        self.nodes["ExtractROI"].process.t_min = 0
+        self.nodes["ExtractROI"].process.t_size = 1
+        self.nodes["ExtractROI"].process.suffix = "gm"
         self.add_process(
             "CoregistrationFlirt",
             "mia_processes.bricks.preprocess.fsl.processes.Flirt",
@@ -78,7 +90,11 @@ class Dwi_tissue_boundaries(Pipeline):
         self.export_parameter(
             "ConvertT1w", "in_file", "in_T1w", is_optional=False
         )
-        self.add_link("in_T1w->CoregistrationFlirt.in_reference_file")
+        self.add_link("Generate5tt.out_file->Convert5ttNIfTI.in_file")
+        self.add_link("Convert5ttNIfTI.out_file->ExtractROI.in_file")
+        self.add_link(
+            "ExtractROI.roi_file->CoregistrationFlirt.in_reference_file"
+        )
         self.export_parameter(
             "Extractb0", "in_file", "in_dwi", is_optional=False
         )
@@ -86,17 +102,19 @@ class Dwi_tissue_boundaries(Pipeline):
             "Generate5ttseed", "out_file", "gmwmSeed_coreg", is_optional=False
         )
         self.add_link("Extractb0.out_file->Meanb0.in_file")
-        self.add_link("Meanb0.out_file->Transformfslconvert.in_file")
         self.add_link("Meanb0.out_file->ConvertMeanb0NIfTI.in_file")
         self.add_link("ConvertT1w.out_file->Generate5tt.in_file")
         self.add_link("ConvertT1w.out_file->Transform_1.in_file")
-        self.add_link("ConvertT1w.out_file->Transformfslconvert.reference")
+        self.add_link("ExtractROI.roi_file->Transformfslconvert.reference")
         self.add_link(
             "CoregistrationFlirt.out_matrix_file->"
             "Transformfslconvert.in_transform"
         )
         self.add_link(
             "Transformfslconvert.out_transform->Transform_2.linear_transform"
+        )
+        self.add_link(
+            "ConvertMeanb0NIfTI.out_file->Transformfslconvert.in_file"
         )
         self.add_link(
             "Transformfslconvert.out_transform->Transform_1.linear_transform"
@@ -126,36 +144,41 @@ class Dwi_tissue_boundaries(Pipeline):
         )
 
         # nodes positions
+        # nodes positions
         self.node_position = {
-            "Generate5ttseed": (572.7923970930416, 563.1519654602814),
-            "Extractb0": (-1005.4957981923569, 417.64372749745445),
-            "Meanb0": (-806.8555420941282, 463.51008970621),
-            "ConvertT1w": (-832.3275799578836, -255.8711697726233),
-            "CoregistrationFlirt": (-290.10564078264713, 750.5788923698742),
-            "Transformfslconvert": (-249.62126489965024, 497.1893676200373),
-            "ConvertMeanb0NIfTI": (-585.5133725344976, 705.5775392569157),
-            "Transform_1": (158.362335819779, -327.46138729475047),
-            "Transform_2": (97.9136867940988, 468.4686461045751),
+            "Generate5ttseed": (733.5035929823755, 496.18896717305904),
+            "Extractb0": (-1013.5313579868234, 229.70091230465),
+            "Meanb0": (-804.1770221626393, 294.3169140338279),
+            "ConvertT1w": (-989.0209959499841, -274.6208092930456),
+            "CoregistrationFlirt": (-182.9648435230911, 517.5476583303403),
+            "Transformfslconvert": (128.05004544028458, 371.2989308400588),
+            "ConvertMeanb0NIfTI": (-574.7992928085422, 453.79666569695905),
+            "Transform_1": (348.53725095549083, -292.6406281853948),
+            "Transform_2": (371.1227198059666, 653.2865213773091),
             "inputs": (-1161.5447656473912, 66.84405832847222),
-            "Generate5tt": (-371.1225123162598, -137.1539719429656),
+            "Generate5tt": (-717.0980034669092, -99.20827291353953),
             "outputs": (791.427723268604, 42.77366014726516),
+            "Convert5ttNIfTI": (-409.79240138257603, -146.41518218711377),
+            "ExtractROI": (-110.24074392890529, -180.34117876155892),
         }
 
         # nodes dimensions
         self.node_dimension = {
-            "ConvertT1w": (228.28125, 460.0),
+            "ConvertT1w": (222.28125, 460.0),
             "dwidenoise_1": (156.234375, 180.0),
-            "Generate5ttseed": (153.359375, 110.0),
-            "Extractb0": (166.375, 215.0),
+            "Generate5ttseed": (146.65625, 110.0),
+            "Extractb0": (163.375, 215.0),
             "Meanb0": (186.96875, 180.0),
-            "CoregistrationFlirt": (257.890625, 215.0),
+            "CoregistrationFlirt": (278.046875, 1580.0),
             "Transformfslconvert": (226.1875, 145.0),
             "ConvertMeanb0NIfTI": (222.28125, 460.0),
             "Transform_1": (220.34375, 600.0),
             "Transform_2": (220.34375, 600.0),
-            "inputs": (106.984375, 145.0),
+            "inputs": (95.26626223929541, 90.0),
             "Generate5tt": (199.46875, 250.0),
-            "outputs": (148.671875, 145.0),
+            "outputs": (151.69617003946317, 115.0),
+            "Convert5ttNIfTI": (222.28125, 460.0),
+            "ExtractROI": (171.046875, 425.0),
         }
 
         self.do_autoexport_nodes_parameters = False
