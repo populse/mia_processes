700edc3b630ac3655ac239f126ae5b0ecd9510f7
econdami <econdami@yahoo.fr>
2023-12-18 13:53:58 +0100

    nake html

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.1-dev+f9b09e87',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.5.1-dev+753b3f76',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
