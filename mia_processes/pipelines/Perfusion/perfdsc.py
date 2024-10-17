41042a871ea7cdaa2ab00e5112922054d6a7a2f5
econdami <econdami@yahoo.fr>
2024-10-17 21:17:34 +0200

    Work on Perdsc pipeline (#80)

--- mia_processes/pipelines/Perfusion/perfdsc.py
+++ mia_processes/pipelines/Perfusion/perfdsc.py
@@ -97,6 +97,10 @@ class Perfdsc(Pipeline):
             "deconv_from_aif",
             "mia_processes.bricks.tools.tools.Deconv_from_aif",
         )
+        self.add_process(
+            "reportperfdsc",
+            "mia_processes.bricks.reports.reporting.ReportPerfDsc",
+        )
 
         # links
         self.export_parameter(
@@ -114,77 +118,78 @@ class Perfdsc(Pipeline):
             "spatial_mask.deformation_file"
         )
         self.add_link(
+            "spatial_preprocessing.realignment_parameters->"
+            "reportperfdsc.realignment_parameters"
+        )
+        self.add_link(
+            "spatial_preprocessing.normalized_anat->" "reportperfdsc.norm_anat"
+        )
+        self.nodes["spatial_preprocessing"].process.trait(
+            "coregistered_source"
+        ).userlevel = 1
+        self.export_parameter(
+            "spatial_preprocessing", "coregistered_source", is_optional=False
+        )
+        self.add_link(
             "spatial_preprocessing.smoothed_func->"
             "spatial_mask.smoothed_func"
-        )
-        self.export_parameter(
-            "spatial_preprocessing",
-            "coregistered_source",
-            is_optional=False,
         )
         self.add_link(
             "spatial_preprocessing.smoothed_func->make_aif.func_file"
         )
         self.add_link(
+            "spatial_preprocessing.normalized_func->" "reportperfdsc.norm_func"
+        )
+        self.add_link(
             "spatial_preprocessing.smoothed_func->deconv_from_aif.func_file"
         )
-        # Only done for normalize12_2 activation!
-        self.export_parameter(
-            "spatial_preprocessing", "normalized_anat", is_optional=False
-        )
         self.add_link("spatial_mask.mask_003->deconv_from_aif.mask_file")
+        self.add_link("make_aif.aif_file->reportperfdsc.aif_file")
         self.add_link("make_aif.aif_file->deconv_from_aif.aif_file")
-        self.export_parameter(
-            "deconv_from_aif", "CBV_image", is_optional=False
-        )
-        self.export_parameter(
-            "deconv_from_aif", "CBF_image", is_optional=False
-        )
-        self.export_parameter(
-            "deconv_from_aif", "MTT_image", is_optional=False
-        )
+        self.add_link("deconv_from_aif.CBV_image->reportperfdsc.CBV_image")
+        self.add_link("deconv_from_aif.CBF_image->reportperfdsc.CBF_image")
+        self.add_link("deconv_from_aif.MTT_image->reportperfdsc.MTT_image")
+        self.add_link("deconv_from_aif.Tmax_image->reportperfdsc.Tmax_image")
+        self.nodes["deconv_from_aif"].process.trait("TTP_image").userlevel = 1
         self.export_parameter(
             "deconv_from_aif", "TTP_image", is_optional=False
         )
-        self.export_parameter(
-            "deconv_from_aif", "Tmax_image", is_optional=False
-        )
+        self.nodes["deconv_from_aif"].process.trait("T0_image").userlevel = 1
         self.export_parameter("deconv_from_aif", "T0_image", is_optional=False)
+        self.export_parameter("reportperfdsc", "report", is_optional=True)
 
         # parameters order
         self.reorder_traits(
             (
                 "anat_file",
                 "func_files",
-                "normalized_anat",
                 "coregistered_source",
-                "CBV_image",
-                "CBF_image",
-                "MTT_image",
                 "TTP_image",
-                "Tmax_image",
                 "T0_image",
+                "report",
             )
         )
 
         # nodes positions
         self.node_position = {
-            "spatial_preprocessing": (-197.948, -382.316),
-            "spatial_mask": (215.557, -349.799),
-            "make_aif": (93.192, 225.635),
-            "deconv_from_aif": (499.156, -27.223),
-            "outputs": (902.105, -79.388),
-            "inputs": (-474.037, -126.341),
+            "spatial_preprocessing": (-324.676, 18.988),
+            "spatial_mask": (64.459, -197.076),
+            "make_aif": (-33.536, 566.825),
+            "deconv_from_aif": (375.678, 268.475),
+            "reportperfdsc": (731.121, -396.430),
+            "inputs": (-519.529, 133.613),
+            "outputs": (1046.820, 25.995),
         }
 
         # nodes dimensions
         self.node_dimension = {
             "spatial_preprocessing": (236.515625, 355.0),
             "spatial_mask": (200.15625, 145.0),
-            "outputs": (138.83679503946317, 236.0),
-            "inputs": (157.4225122392954, 286.0),
             "make_aif": (172.171875, 250.0),
-            "deconv_from_aif_1": (215.328125, 320.0),
+            "deconv_from_aif": (215.328125, 320.0),
+            "reportperfdsc": (212.484375, 1265.0),
+            "inputs": (96.79751223929541, 110.0),
+            "outputs": (59.578125, 75.0),
         }
 
         self.do_autoexport_nodes_parameters = False
